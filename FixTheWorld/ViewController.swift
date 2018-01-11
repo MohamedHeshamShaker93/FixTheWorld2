e1f6717d6d93eb1944ba2cc674fb63448dbbbcc8
MohamedHeshamShaker93 <mohamedh2099@gmail.com>
2018-01-11 12:21:25 +0200

    Hello World

---
+++ FixTheWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FixTheWorld
+//
+//  Created by Mohammed Hesham on 1/11/18.
+//  Copyright © 2018 Mohammed Hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
